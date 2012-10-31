c577d36c5c22abfbd14198190d3d62247878a515
Olav Frengstad <olav@fwt.no>
2012-10-31 16:17:48 +0100

    Remove unused files

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup(name='guri',
-      version='1.0',
-      py_modules=['guri'],
-      requires = ["serial", "binascii"],
-      author="Olav Frengstad",
-      author_email="olav@tiny-mesh.com",
-      description="Simple serial to TCP/IP communications for TinyMesh devices",
-      )
